b1b84b519cd5659c4c4b033a9b3a3ca9bfe7b81f
Mahesh Arya <mahesharya@prmanicus.com>
2026-05-25 17:48:59 +0530

    c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main() {
+    printf("Hello world\n");
+    printf("Welcome to C programming classes\n");
+    printf("%d ",3+4);
+    printf("%d ",3-4);
+    printf("%d ",3*4);
+    printf("%d ",4/2);
+    printf("%d ",4%2); //reminder
+    //arithmetic operators
+    //+,-,*,/,%
+}
