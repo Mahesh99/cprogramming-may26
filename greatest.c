5329b51da6e1e3b5e7acb0949d41addb8a1a7403
Mahesh Arya <mahesharya@prmanicus.com>
2026-05-29 18:16:16 +0530

    c

---
+++ greatest.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main() {
+    int a,b,c;
+    printf("Enter value of a,b,c:");
+    scanf("%d%d%d",&a,&b,&c);
+    
+}
---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() {
+    int a=110;
+    printf("%d",a<<2);
+    printf("%c",a);
+    printf("%d",6&3);
+    printf("%d",3>2);
+    printf("%d",3<2);
+    printf("%d",sizeof a);
+}
